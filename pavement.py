8201e065158700c4dc7c239fd0b4bdffebc210c7
Christian Fobel <christian@fobel.net>
2014-08-27 13:03:33 -0400

    Add pip_helpers package to install list

--- pavement.py
+++ pavement.py
@@ -11,7 +11,8 @@ setup(name='pip_helpers',
       author_email='christian@fobel.net',
       url='http://github.com/wheeler-microfluidics/pip_helpers.git',
       license='GPLv2',
-      install_requires=['pip'])
+      install_requires=['pip'],
+      packages=['pip_helpers'])
 
 
 @task
